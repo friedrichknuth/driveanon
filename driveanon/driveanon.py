db4df9338ff1b30d54b77305892d8bec9db5f93a
Friedrich Knuth <friedrichknuth@users.noreply.github.com>
2019-02-20 15:34:19 -0800

    Add folder comprehension

--- driveanon/driveanon.py
+++ driveanon/driveanon.py
@@ -1,5 +1,6 @@
 import io
 import requests
+from bs4 import BeautifulSoup
 
 def _get_token(response):
     for key, value in response.cookies.items():
@@ -57,3 +58,42 @@ def save(blob_id, filename = None, overwrite = False):
         w.write(response.content)
 
     return filename
+
+def request_folder_blob(folder_blob_id):
+    url = "https://drive.google.com/drive/folders/%s" % folder_blob_id
+    session = requests.Session()
+    response = session.get(url, params = { 'usp' : 'sharing' })
+    html_response = BeautifulSoup(response.text, 'html.parser')
+    return html_response
+
+def find_content_block(html_response, extension):
+    content_block = []
+    for element in html_response.find_all('script'):
+        if extension in element.text:
+            content_block.append(element)
+    return content_block
+
+def extract_file_indices(content_block, extension):
+    # split up content up by common seperating character string
+    all_elements = str(content_block[0]).split('\\x22')
+    # extract indices for each extension occurance
+    file_indices = [i for i, s in enumerate(all_elements) if extension in s]
+    return file_indices, all_elements
+
+def get_file_blobs(all_elements, file_indices):
+    file_names = []
+    blob_ids = []
+    # iterate over indices of file names
+    for ind in file_indices:
+        # extract file name
+        file_names.append(all_elements[ind])
+        # extract blob id, which occurs 4 indices before the file name
+        blob_ids.append(all_elements[ind-4])
+    return file_names, blob_ids
+
+def list_blobs(folder_blob_id,extension):
+    html_response = request_folder_blob(folder_blob_id)
+    content_block = find_content_block(html_response, extension)
+    file_indices, all_elements = extract_file_indices(content_block, extension)
+    file_names, file_blob_ids = get_file_blobs(all_elements, file_indices)
+    return file_blob_ids
